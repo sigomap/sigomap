b4d4dd26092c7aed932f7da6ff9d1936abccb954
Sigomap <sigomapdevelopper@gmail.com>
2024-10-04 14:38:59 +0000

    first commit

---
+++ src/main/java/ci/dgmp/sigomap/SigomapApplication.java
@@ -0,0 +1,13 @@
+package ci.dgmp.sigomap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SigomapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SigomapApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ci/dgmp/sigomap/SigomapApplicationTests.java
@@ -0,0 +1,13 @@
+package ci.dgmp.sigomap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SigomapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
